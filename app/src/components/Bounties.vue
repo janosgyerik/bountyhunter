302f2790d9feb48825328d91349297ba255eab69
Janos Gyerik <janos@kronos>
2017-12-01 21:45:22 +0100

    Make it easier to see bounties with their potential

--- app/src/components/Bounties.vue
+++ app/src/components/Bounties.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="questions">
-    <p><input v-model="tagsInput" v-on:change="applyFilters"></p>
+    <p><input v-model="tagsInput" v-on:change="applyFilters" size="80"></p>
     <h1>Questions <button v-on:click="refresh">Refresh</button></h1>
-    <div v-for="q in questions" class="question">
+    <div v-for="q in questions" class="question" v-bind:class="q.potentialClass">
       <h2><a :href="q.url">{{ q.title }}</a></h2>
       <p><span v-for="tag in q.tags" class="tag">{{ tag }}</span></p>
-      <p>score: {{ q.score }} views: {{ q.views }} accepted: {{ q.accepted }}
-          value: {{ q.bounty.value }} op: {{ q.owner.rep }} b: {{ q.bountyOwner.rep }}
-          answers: <span v-for="a in q.answers">({{ a.score }} by {{ a.owner.rep }})</span></p>
-      <p>date: {{ q.date }} bounty date: {{ q.bounty.endDate }}</p>
+      <p>
+        days: {{ q.days }}
+        views: {{ q.views }}
+        answers: {{ q.answers }}
+        score: {{ q.score }}
+        op: {{ q.owner.rep }}
+        value: {{ q.bounty.value }}
+      </p>
     </div>
   </div>
 </template>
@@ -19,37 +23,44 @@
 import sample1 from '../../static/sample1.json'
 import sample2 from '../../static/sample2.json'
 
+// other possibly interesting fields:
+// - last_activity_date
+// - last_edit_date
 const mapper = (q) => {
+  const date = new Date(1000 * q.creation_date)
+  const days = Math.floor((new Date() - date) / 3600 / 24 / 1000)
+  const accepted = q.accepted_answer_id !== undefined
+  const answers = q.answer_count
+
+  var potentialClass = ''
+  if (accepted) {
+    potentialClass = 'potential-none'
+  } else if (answers === 0) {
+    potentialClass = 'potential-high'
+  } else if (answers > 1) {
+    potentialClass = 'potential-low'
+  }
+
   return {
     title: q.title,
     url: q.link,
     tags: q.tags,
     score: q.score,
-    date: new Date(1000 * q.creation_date),
-    // last_activity_date, last_edit_date
-    stars: -1,
+    date: date,
+    days: days,
     views: q.view_count,
-    accepted: q.accepted_answer_id !== undefined,
-    answers: Array.from({ length: q.answer_count }).map(() => {
-      return {
-        score: -99,
-        date: new Date(),
-        accepted: false,
-        owner: {}
-      }
-    }),
+    accepted: accepted,
+    answers: answers,
     bounty: {
       value: q.bounty_amount,
-      text: '?',
-      customText: '?',
-      endDate: new Date(1000 * q.bounty_closes_date)
+      date: new Date(1000 * q.bounty_closes_date)
     },
     owner: {
       url: q.owner.link,
       name: q.owner.display_name,
       rep: q.owner.reputation
     },
-    bountyOwner: {}
+    potentialClass: potentialClass
   }
 }
 
@@ -62,7 +73,7 @@ const applyFilters = (that) => {
 
   that.questions = Object.values(that.repo)
     .filter(q => q.tags.some(tag => tags.has(tag)))
-    .sort((q1, q2) => q2.bounty.endDate - q1.bounty.endDate)
+    .sort((q1, q2) => q2.bounty.date - q1.bounty.date)
 }
 
 export default {
@@ -71,7 +82,7 @@ export default {
     return {
       repo: {},
       questions: [],
-      tagsInput: 'java bash sonarqube'
+      tagsInput: 'java bash sonarqube awk git shell javascript maven sonarlint r unix go python junit'
     }
   },
   mounted () {
@@ -97,5 +108,23 @@ export default {
 <style scoped>
 .tag {
   padding: .5em;
+}
+
+.question {
+  border: 1px dashed silver;
+  margin: .5em;
+}
+
+.potential-high {
+  background-color: #fee;
+}
+
+.potential-low, .potential-low a {
+  background-color: #eee;
+  color: #ccc;
+}
+
+.potential-none {
+  display: none;
 }
 </style>
